1e78a5e0f331c58ddb1dd18363477e0d573df32d
Jonti Hudson <33636074+JontiHudson@users.noreply.github.com>
2022-01-13 09:07:18 +0000

    1.6.0-beta46

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createInheritableComponent = void 0;
-const createComponent_1 = require("./createComponent");
-function createInheritableComponent(Component) {
-    return (0, createComponent_1.createComponent)(Component, {});
-}
-exports.createInheritableComponent = createInheritableComponent;
